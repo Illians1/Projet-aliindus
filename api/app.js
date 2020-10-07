d3fccedd902211992cd1e97103ce0152c00ecf11
Mehdi <aouidamehdi@gmail.com>
2020-10-07 09:42:36 +0200

    debut front

--- api/app.js
+++ api/app.js
@@ -1,33 +1,57 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'aliindustrie'
+  host: "127.0.0.1",
+  user: "root",
+  password: "",
+  database: "aliindustrie",
 });
 
 connection.connect(function (err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
 
-    console.log('connected as id ' + connection.threadId);
+  console.log("connected as id " + connection.threadId);
 });
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
 
-
+app.get("/", (req, res, next) => {
+  connection.query(
+    'SELECT * FROM `clients` WHERE code_postal LIKE "%863%"',
+    function (error, results, fields) {
+      // error will be an Error if one occurred during the query
+      // results will contain the results of the query
+      // fields will contain information about the returned results fields (if any)
+      if (error) {
+        res.status(400).json({
+          error,
+        });
+      } else {
+        res.status(200).json({
+          results,
+        });
+      }
+    }
+  );
+});
 
 module.exports = app;
